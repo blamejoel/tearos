ddcc5625abb9aa9236aed0f965d20912f7fb5a76
Joel Gomez <joel.gomez@email.ucr.edu>
2016-06-03 14:53:47 -0700

    some cleanup on semaphore tests

--- test1_3c.c
+++ test1_3c.c
@@ -1,26 +1,25 @@
 #include "types.h"
 #include "user.h"
 #include "semaphore.h"
+
 #define TEST_NUM 21
 
-int numCan, numMiss;
+int count_cann, count_mission;
 Semaphore mutex, cannibal, missionary, row;
 
-void RowBoat() {
+void row_boat() {
     printf(1, "Rowing!\n");
 }
 
 int num_threads;
 
-void MissionaryArrives(void *arg); 
-void CannibalArrives(void *arg); 
+void arrival_missionary(void *arg); 
+void arrival_cannibal(void *arg); 
 
 
 int main() {
 
-// like this
-
-    numCan = numMiss = num_threads = 0;
+    count_cann = count_mission = num_threads = 0;
 
     num_threads = 0;
     sem_init(&mutex, 1);
@@ -31,11 +30,11 @@ int main() {
     int i;
     int *tid;
 
-    printf(1, "Testing with %d randomly generated canibals and missionaries\n", TEST_NUM);
-    for (i = 1; i < TEST_NUM+1; i++) {
-        if ((random(99999*i)/101)%2) {
-            tid = thread_create(CannibalArrives, (void*)0);
-            if (tid == 0) {
+    printf(1, "Testing with %d randomly generated cannibals and missionaries\n", TEST_NUM);
+    for(i = 1; i < TEST_NUM+1; i++) {
+        if((random(99999*i)/101)%2) {
+            tid = thread_create(arrival_cannibal, (void*)0);
+            if(tid == 0) {
                 printf(1, "failed to create thread!\n");
                 exit();
             }
@@ -44,8 +43,8 @@ int main() {
             sem_signal(&mutex);
         }
         else {
-            tid = thread_create(MissionaryArrives, (void*)0);
-            if (tid == 0) {
+            tid = thread_create(arrival_missionary, (void*)0);
+            if(tid == 0) {
                 printf(1, "failed to create thread!\n");
             }
             sem_acquire(&mutex);
@@ -53,9 +52,9 @@ int main() {
             sem_signal(&mutex);
         }
     }
-    for (i = 0; i < 3; i++){
-        tid = thread_create(MissionaryArrives, (void*)0);
-        if (tid == 0) {
+    for(i = 0; i < 3; i++){
+        tid = thread_create(arrival_missionary, (void*)0);
+        if(tid == 0) {
             printf(1, "failed to create thread!\n");
         }
         sem_acquire(&mutex);
@@ -67,22 +66,22 @@ int main() {
     while(wait()>0);
     exit();
 }
-void CannibalArrives(void *arg) {
+void arrival_cannibal(void *arg) {
     sem_acquire(&mutex);
-    if (numCan >= 2) {
-        numCan -= 2;
+    if(count_cann >= 2) {
+        count_cann -= 2;
         sem_signal(&mutex);
         sem_signal(&cannibal);
         sem_signal(&cannibal);
     }
-    else if (numMiss >= 2) {
-        numMiss -= 2;
+    else if(count_mission >= 2) {
+        count_mission -= 2;
         sem_signal(&mutex);
         sem_signal(&missionary);
         sem_signal(&missionary);
     }
     else {
-        numCan++;
+        count_cann++;
         sem_signal(&mutex);
         sem_acquire(&cannibal);
         sem_acquire(&row);
@@ -93,7 +92,7 @@ void CannibalArrives(void *arg) {
         sem_signal(&mutex);
         texit();
     }
-    RowBoat();
+    row_boat();
     sem_signal(&row);
     sem_signal(&row);
     sem_acquire(&mutex);
@@ -105,23 +104,23 @@ void CannibalArrives(void *arg) {
 }
 
 
-void MissionaryArrives(void *arg) {
+void arrival_missionary(void *arg) {
     sem_acquire(&mutex);
-    if (numMiss >= 2) {
-        numMiss -= 2;
+    if(count_mission >= 2) {
+        count_mission -= 2;
         sem_signal(&mutex);
         sem_signal(&missionary);
         sem_signal(&missionary);
     }
-    else if (numMiss >= 1 && numCan >= 1) {
-        numMiss--;;
-        numCan--;
+    else if(count_mission >= 1 && count_cann >= 1) {
+        count_mission--;;
+        count_cann--;
         sem_signal(&mutex);
         sem_signal(&missionary);
         sem_signal(&cannibal);
     }
     else {
-        numMiss++;
+        count_mission++;
         sem_signal(&mutex);
         sem_acquire(&missionary);
         sem_acquire(&row);
@@ -132,7 +131,7 @@ void MissionaryArrives(void *arg) {
         sem_signal(&mutex);
         texit();
     }
-    RowBoat();
+    row_boat();
     sem_signal(&row);
     sem_signal(&row);
     sem_acquire(&mutex);
