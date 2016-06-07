ab3c68b51f82ed22c97ae104e3412dbe529b3ba3
Joel Gomez <joel.gomez@email.ucr.edu>
2016-06-07 16:28:13 -0700

    cleaned up test1_3c

--- test1_3c.c
+++ test1_3c.c
@@ -2,70 +2,15 @@
 #include "user.h"
 #include "semaphore.h"
 
-#define TEST_NUM 21
+#define TEST_CASE 21
 
-int count_cann, count_mission;
+int count_cann, count_mission, thread_num;
 Semaphore mutex, cannibal, missionary, row;
 
 void row_boat() {
-    printf(1, "Rowing!\n");
+    printf(1, "Boat is ready, rowing!\n");
 }
 
-int num_threads;
-
-void arrival_missionary(void *arg); 
-void arrival_cannibal(void *arg); 
-
-
-int main() {
-
-    count_cann = count_mission = num_threads = 0;
-
-    num_threads = 0;
-    sem_init(&mutex, 1);
-    sem_init(&cannibal, 0);
-    sem_init(&missionary, 0);
-    sem_init(&row, 0);
-
-    int i;
-    int *tid;
-
-    printf(1, "Testing with %d randomly generated cannibals and missionaries\n", TEST_NUM);
-    for(i = 1; i < TEST_NUM+1; i++) {
-        if((random(99999*i)/101)%2) {
-            tid = thread_create(arrival_cannibal, (void*)0);
-            if(tid == 0) {
-                printf(1, "failed to create thread!\n");
-                exit();
-            }
-            sem_acquire(&mutex);
-            num_threads++;
-            sem_signal(&mutex);
-        }
-        else {
-            tid = thread_create(arrival_missionary, (void*)0);
-            if(tid == 0) {
-                printf(1, "failed to create thread!\n");
-            }
-            sem_acquire(&mutex);
-            num_threads++;
-            sem_signal(&mutex);
-        }
-    }
-    for(i = 0; i < 3; i++){
-        tid = thread_create(arrival_missionary, (void*)0);
-        if(tid == 0) {
-            printf(1, "failed to create thread!\n");
-        }
-        sem_acquire(&mutex);
-        num_threads++;
-        sem_signal(&mutex);
-    }
-
-    printf(1, "exiting!");
-    while(wait()>0);
-    exit();
-}
 void arrival_cannibal(void *arg) {
     sem_acquire(&mutex);
     if(count_cann >= 2) {
@@ -86,9 +31,9 @@ void arrival_cannibal(void *arg) {
         sem_acquire(&cannibal);
         sem_acquire(&row);
         sem_acquire(&mutex);
-        printf(1, "before: %d", num_threads);
-        num_threads--;
-        printf(1, "  after: %d\n", num_threads);
+        printf(1, "before: %d, ", thread_num);
+        thread_num--;
+        printf(1, "after: %d\n", thread_num);
         sem_signal(&mutex);
         texit();
     }
@@ -96,13 +41,12 @@ void arrival_cannibal(void *arg) {
     sem_signal(&row);
     sem_signal(&row);
     sem_acquire(&mutex);
-    printf(1, "before: %d", num_threads);
-    num_threads--;
-    printf(1, "  after: %d\n", num_threads);
+    printf(1, "before: %d, ", thread_num);
+    thread_num--;
+    printf(1, "after: %d\n", thread_num);
     sem_signal(&mutex);
     texit();
 }
-
 
 void arrival_missionary(void *arg) {
     sem_acquire(&mutex);
@@ -125,9 +69,9 @@ void arrival_missionary(void *arg) {
         sem_acquire(&missionary);
         sem_acquire(&row);
         sem_acquire(&mutex);
-        printf(1, "before: %d", num_threads);
-        num_threads--;
-        printf(1, "  after: %d\n", num_threads);
+        printf(1, "before: %d, ", thread_num);
+        thread_num--;
+        printf(1, "after: %d\n", thread_num);
         sem_signal(&mutex);
         texit();
     }
@@ -135,10 +79,62 @@ void arrival_missionary(void *arg) {
     sem_signal(&row);
     sem_signal(&row);
     sem_acquire(&mutex);
-    printf(1, "before: %d", num_threads);
-    num_threads--;
-    printf(1, "  after: %d\n", num_threads);
+    printf(1, "before: %d, ", thread_num);
+    thread_num--;
+    printf(1, "after: %d\n", thread_num);
     sem_signal(&mutex);
     texit();
 }
 
+int main() {
+
+    count_cann = count_mission = thread_num = 0;
+
+    thread_num = 0;
+    sem_init(&mutex, 1);
+    sem_init(&cannibal, 0);
+    sem_init(&missionary, 0);
+    sem_init(&row, 0);
+
+    int i;
+    int *tid;
+
+    printf(1, "Testing with %d randomly generated cannibals and missionaries\n", 
+        TEST_CASE);
+    for(i = 1; i < TEST_CASE+1; i++) {
+        if((random(99999*i)/101)%2) {
+            tid = thread_create(arrival_cannibal, (void*)0);
+            if(tid == 0) {
+                printf(1, "something went wrong with thread creation!\n");
+                exit();
+            }
+            sem_acquire(&mutex);
+            thread_num++;
+            sem_signal(&mutex);
+        }
+        else {
+            tid = thread_create(arrival_missionary, (void*)0);
+            if(tid == 0) {
+              printf(1, "something went wrong with thread creation!\n");
+            }
+            sem_acquire(&mutex);
+            thread_num++;
+            sem_signal(&mutex);
+        }
+    }
+    for(i = 0; i < 3; i++){
+        tid = thread_create(arrival_missionary, (void*)0);
+        if(tid == 0) {
+          printf(1, "something went wrong with thread creation!\n");
+        }
+        sem_acquire(&mutex);
+        thread_num++;
+        sem_signal(&mutex);
+    }
+
+    printf(1, "done!");
+    while(wait()>0);
+    exit();
+
+    return 0;
+}
